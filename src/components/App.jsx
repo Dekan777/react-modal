3536aa11ad903e0720172050d7ce458b42ab6ea9
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-02-04 00:14:08 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,40 +16,22 @@ const customStyles = {
 Modal.setAppElement('#root');
 
 export const App = () => {
-  let subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+  // let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(true);
 
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
   return (
     <div className={css.container}>
-      <button onClick={openModal}>Open Modal</button>
       <Modal
         isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
+        // onAfterOpen={afterOpenModal}
+        // onRequestClose={}
         style={customStyles}
         contentLabel="Example Modal"
       >
-        <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-        <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
         <form>
           <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
+
+          <button>Search</button>
         </form>
       </Modal>
     </div>
